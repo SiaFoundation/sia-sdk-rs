363eb28082e131e7428d4c27a395aae6beeae083
Chris Schinnerl <chris@sia.tech>
2025-08-15 19:34:33 +0200

    ignore certificate

--- sia_sdk/src/bin/rpc_settings_test.rs
+++ sia_sdk/src/bin/rpc_settings_test.rs
@@ -4,20 +4,23 @@ use web_transport;
 
 #[tokio::main]
 async fn main() {
-    let client = web_transport::ClientBuilder::default()
-        .with_system_roots()
-        .unwrap();
+    let client = unsafe {
+        web_transport::quinn::ClientBuilder::default().with_no_certificate_verification()
+    }
+    .unwrap();
 
-    let mut connection = client
+    let session = client
         .connect(
-            "https://2l8dvkho84abi14l313c4r48267fhffujsbk9hkvproe1rmkhme0.sia.host:9984/sia/rhp/v4"
+            "https://127.0.0.1:49829/sia/rhp/v4"
+                //"https://2l8dvkho84abi14l313c4r48267fhffujsbk9hkvproe1rmkhme0.sia.host:9984/sia/rhp/v4"
                 .parse()
                 .unwrap(),
         )
         .await
         .expect("failed to establish connection");
 
-    let (sender, receiver) = connection.open_bi().await.unwrap();
+    let mut session = web_transport::Session::from(session);
+    let (sender, receiver) = session.open_bi().await.unwrap();
 
     let stream = WebtransportStream::new(sender, receiver);
 
