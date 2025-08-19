f7bf76e6618abb9e4249c7025cf032d5a56925da
Chris Schinnerl <chris@sia.tech>
2025-08-19 18:43:47 +0200

    add offset

--- sia/src/encryption.rs
+++ sia/src/encryption.rs
@@ -1,17 +1,21 @@
 use std::u8;
 
 use chacha20::XChaCha20;
-use chacha20::cipher::{KeyIvInit, StreamCipher};
+use chacha20::cipher::{KeyIvInit, StreamCipher, StreamCipherSeek};
 
 /// encrypts the provided shards using XChaCha20. To decrypt the shards, call
 /// this function again with the same key.
+/// NOTE: don't reuse the same key for the same set of shads as it will
+/// compromise the security of the encryption. Always use a freshly generated
+/// key.
 #[allow(dead_code)]
-pub(crate) fn encrypt_shards(key: &[u8; 32], shards: &mut [Vec<u8>]) {
+pub(crate) fn encrypt_shards(key: &[u8; 32], shards: &mut [Vec<u8>], offset: usize) {
     assert!(shards.len() <= u8::MAX as usize);
     let mut nonce = [0u8; 24]; // XChaCha20 nonce size
     for (i, shard) in shards.iter_mut().enumerate() {
         nonce[0] = i as u8;
         let mut cipher = XChaCha20::new(key.into(), &nonce.into());
+        cipher.seek(offset);
         cipher.apply_keystream(shard);
     }
 }
@@ -26,12 +30,22 @@ mod test {
         let mut shards = vec![vec![1, 2, 3], vec![4, 5, 6]];
 
         // encrypt
-        encrypt_shards(&key, &mut shards);
+        encrypt_shards(&key, &mut shards, 0);
         assert_eq!(shards[0], vec![136, 154, 188]);
         assert_eq!(shards[1], vec![70, 216, 180]);
 
         // decrypt
-        encrypt_shards(&key, &mut shards);
+        encrypt_shards(&key, &mut shards, 0);
+        assert_eq!(shards[0], vec![1, 2, 3]);
+        assert_eq!(shards[1], vec![4, 5, 6]);
+
+        // encrypt with offset
+        encrypt_shards(&key, &mut shards, 100);
+        assert_eq!(shards[0], vec![6, 194, 192]);
+        assert_eq!(shards[1], vec![236, 188, 165]);
+
+        // decrypt with offset
+        encrypt_shards(&key, &mut shards, 100);
         assert_eq!(shards[0], vec![1, 2, 3]);
         assert_eq!(shards[1], vec![4, 5, 6]);
     }
