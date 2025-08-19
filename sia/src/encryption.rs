ddc52634b2746c295b7c5a6c1d72c766bb7d89b0
Nate <me@n8m.us>
2025-08-19 11:51:13 -0700

    add slab impl

--- sia/src/encryption.rs
+++ sia/src/encryption.rs
@@ -6,16 +6,28 @@ use chacha20::cipher::{KeyIvInit, StreamCipher, StreamCipherSeek};
 /// NOTE: don't reuse the same key for the same set of shads as it will
 /// compromise the security of the encryption. Always use a freshly generated
 /// key.
-#[allow(dead_code)]
-pub(crate) fn encrypt_shards(key: &[u8; 32], shards: &mut [Vec<u8>], offset: usize) {
+pub(crate) fn encrypt_shards<A: AsMut<[u8]>>(key: &[u8; 32], shards: &mut [A], offset: usize) {
     assert!(shards.len() <= u8::MAX as usize);
     let mut nonce = [0u8; 24]; // XChaCha20 nonce size
     for (i, shard) in shards.iter_mut().enumerate() {
         nonce[0] = i as u8;
         let mut cipher = XChaCha20::new(key.into(), &nonce.into());
         cipher.seek(offset);
-        cipher.apply_keystream(shard);
+        cipher.apply_keystream(shard.as_mut());
     }
+}
+
+/// encrypts the provided shard using XChaCha20. To decrypt the shard, call
+/// this function again with the same key.
+/// NOTE: don't reuse the same key for the same set of shads as it will
+/// compromise the security of the encryption. Always use a freshly generated
+/// key.
+pub(crate) fn encrypt_shard<A: AsMut<[u8]>>(key: &[u8; 32], shard: &mut A, index: u8, offset: usize) {
+    let mut nonce = [0u8; 24]; // XChaCha20 nonce size
+    nonce[0] = index;
+    let mut cipher = XChaCha20::new(key.into(), &nonce.into());
+    cipher.seek(offset);
+    cipher.apply_keystream(shard.as_mut());
 }
 
 #[cfg(test)]
