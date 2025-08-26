58c8b20340b594a8980981ddaa9cecf14458f2b4
Chris Schinnerl <3903476+ChrisSchinnerl@users.noreply.github.com>
2025-08-26 14:33:06 +0200

    implement parse for public key

--- sia/src/macros.rs
+++ sia/src/macros.rs
@@ -80,7 +80,7 @@ macro_rules! impl_hash_id {
                 };
 
                 if s.len() != 64 {
-                    return Err($crate::types::HexParseError::InvalidLength);
+                    return Err($crate::types::HexParseError::InvalidLength(s.len()));
                 }
 
                 let mut data = [0u8; 32];
