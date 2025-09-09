ee707e235a3db2261f8cf588948ef91359035131
PJ <peterjan.brone@gmail.com>
2025-09-09 13:47:19 -0700

    indexd_ffi: update examples

--- indexd_ffi/examples/python/test.py
+++ indexd_ffi/examples/python/test.py
@@ -1,5 +1,5 @@
 import asyncio
-from indexd import App, Logger, set_log_callback
+from indexd_ffi import App, Logger, set_log_callback
 
 class PrintLogger(Logger):
     def debug(self, msg: "str"):
@@ -17,12 +17,22 @@ async def main():
     await app.connect()
     print("Connected to indexd")
 
-    upload = await app.upload(b'\x01' * 32, 2, 6)
+    upload = await app.upload(b'\x01' * 32, 1, 2)
     print("starting chunked upload")
     await upload.write(b'\x02' * 1024)
     print("Wrote 1024 bytes")
-    await upload.finish()
-    print("Upload finished")
 
+    slabs = await upload.finish()
+    print("Upload finished")
+    for slab in slabs:
+        print("Slab ID:", slab.id)
+
+    download = await app.download(slabs)
+    while True:
+        chunk = await download.read(512)
+        print("Read chunk of size:", len(chunk))
+        if chunk == b'':
+            break
+    print("Download finished")
 
 asyncio.run(main())
