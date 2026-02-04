557a3dbfe3893374451e8237378ca879d8592ff9
Alright <mzlot555@gmail.com>
2026-02-04 12:20:11 -0500

    add Hosts::available_for_upload; add doc comment for Hosts::available

--- indexd/src/hosts.rs
+++ indexd/src/hosts.rs
@@ -249,9 +249,20 @@ impl Hosts {
         }
     }
 
+    /// Returns the total number of known hosts (including those not good for upload).
     pub fn available(&self) -> usize {
         let inner = self.inner.read().unwrap();
         inner.hosts.len()
+    }
+
+    /// Returns the number of known hosts that are good for upload.
+    pub fn available_for_upload(&self) -> usize {
+        let inner = self.inner.read().unwrap();
+        inner
+            .hosts
+            .iter()
+            .filter(|(_, h)| h.good_for_upload)
+            .count()
     }
 
     /// Returns a list of all known hosts, sorted by priority.
