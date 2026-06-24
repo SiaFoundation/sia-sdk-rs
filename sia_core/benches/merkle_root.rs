cc48fd56ee6e63e6ddfe6758ef77e5893c1a0103
Nate <me@n8m.us>
2026-06-24 15:42:49 -0700

    ci: test SIMD

--- sia_core/benches/merkle_root.rs
+++ sia_core/benches/merkle_root.rs
@@ -3,7 +3,7 @@ use sia_core::rhp4::SECTOR_SIZE;
 use std::hint::black_box;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let sector = vec![0u8; 1 << 22];
+    let sector = vec![0u8; SECTOR_SIZE];
     let mut group = c.benchmark_group("sector_root");
     group.throughput(Throughput::Bits(SECTOR_SIZE as u64 * 8));
     group.bench_function("sector_root", |b| {
